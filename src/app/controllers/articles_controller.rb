1e25a11ffa29f31d7d0b5494741376af8245c87a
student1 <student1@hpc285.tcnj.edu>
2024-03-26 21:46:39 -0400

    Prototype3

--- src/app/controllers/articles_controller.rb
+++ src/app/controllers/articles_controller.rb
@@ -1,4 +1,29 @@
 class ArticlesController < ApplicationController
+
+ #authenticating and keeping user dashbpards seperate on sign in
+  before_action :authenticate_user!, only: [:users]
+
+  before_action :authenticate_admin!, only: [:admin]
+  before_action :authenticate_healthcare!, only: [:healthcare]
+
+  def symptoms
+  end
+
+
+  def about
+  end
+
+
   def index
+  end
+
+
+  def users
+  end
+
+  def healthcare
+  end
+
+  def admin
   end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,13 +1,39 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  get "/articles", to: "articles#index"
+  devise_for :healthcares
+
+
+  devise_for :admin
+
   devise_for :users
+# Define routes for the about page
+get '/about', to: 'about#show', as: 'about_page'
+
+# Define routes for the symptoms page
+get '/symptoms', to: 'articles#symptoms', as: 'symptoms_page'
+
+# Root route
+root "articles#index"
+get '/index', to: 'articles#index', as: 'index_page'
+
+# Define routes for articles
+get '/articles', to: 'articles#index'
+
+# Define routes for different user types
+get '/admin', to: 'articles#admins', as: 'admin_page'
+get '/users', to: 'articles#users', as: 'users_page'
+get '/healthcare', to: 'articles#healthcares', as: 'healthcarepro_page'
+
+
+
+# Define routes for different admin login
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get '/fetch_symptoms', to: 'symptoms#fetch_symptoms'
+
 
   # Defines the root path route ("/")
   # root "posts#index"
