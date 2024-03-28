e3a4f78c2743b9db1c3f93ff4e3fc9f16d40a8f8
student1 <student1@hpc285.tcnj.edu>
2024-03-27 21:49:36 -0400

    -select to api

--- src/app/controllers/articles_controller.rb
+++ src/app/controllers/articles_controller.rb
@@ -7,6 +7,27 @@ class ArticlesController < ApplicationController
   before_action :authenticate_healthcare!, only: [:healthcare]
 
   def symptoms
+      url = URI("https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImV2cmFhMUB0Y25qLmVkdSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTM3MzYiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjQtMDMtMjgiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTcxMTU5NDY1NiwibmJmIjoxNzExNTg3NDU2fQ.wP-ReKQFPrwHJGxrFsdfr6Z6377HVYHjLCEOa2yr26E&format=json&language=en-gb")
+
+
+    @res = Net::HTTP.get_response(url)
+    #puts @res.body
+    @temp = JSON.parse(@res.body)
+
+    @temp.each do |val|
+      print val
+      puts "\n"
+      print val["Name"]
+      puts "\n"
+    end
+    #length = temp
+    #for entry in temp do
+
+     # puts entry
+
+
+
+    #end
   end
 
 
--- src/app/controllers/symptoms_controller.rb
+++ src/app/controllers/symptoms_controller.rb
@@ -1,14 +1,10 @@
 require 'uri'
 require 'net/http'
 
-url = URI("https://diagnosis.p.rapidapi.com/api/DDxItems/GetSymptoms?AuthenticationID=DEMO_AuthenticationID")
+def show
+  url = URI("https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImV2cmFhMUB0Y25qLmVkdSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTM3MzYiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjQtMDMtMjgiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTcxMTU5NDY1NiwibmJmIjoxNzExNTg3NDU2fQ.wP-ReKQFPrwHJGxrFsdfr6Z6377HVYHjLCEOa2yr26E&format=json&language=en-gb")
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
-request["X-RapidAPI-Key"] = '295a130e1bmshcdaf6dae8992433p140e63jsn55a74ff77d09'
-request["X-RapidAPI-Host"] = 'diagnosis.p.rapidapi.com'
-
-response = http.request(request)
-puts response.read_body
+  @res = Net::HTTP.get_response(url)
+  puts @res
+end
